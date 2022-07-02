33d0b961983bf2ae348811973c3eb53254c82900
m-onimura <onimu99@gmail.com>
2022-07-03 03:12:46 +0900

    modify comment

--- init.el
+++ init.el
@@ -1,30 +1,29 @@
-; replace C-h with backspace
+; set backspace to C-h
 (global-set-key "\C-h" `delete-backward-char)
 ; not create backup(foo.txt~)
 (setq make-backup-files nil)
 ; not create backup("#foo.txt#")
 (setq auto-save-default nil)
-; replace C-c with kill-ring-save(copy)
+; set wsl-copy to C-c
 (defun wsl-copy (start end)
   (interactive "r")
   (shell-command-on-region start end "clip.exe")
   (deactivate-mark))
-;(global-set-key "\C-c" `kill-ring-save)
 (global-set-key "\C-c" `wsl-copy)
-; replace C-x x with kill-region(cut)
+; set wsl-cut to C-x C-x
 (defun wsl-cut (start end)
   (interactive "r")
   (shell-command-on-region start end "clip.exe")
   (kill-region start end))
 (global-set-key "\C-x\C-x" `wsl-cut)
-; replace C-f C-f with isearch-forward(search)
+; set isearch-forward to C-f C-f
 (global-set-key "\C-x\C-f" `isearch-forward)
-; replace C-s with save-buffer
+; set save-buffer to C-s
 (global-set-key "\C-s" `save-buffer)
 ; set window move
 (global-set-key (kbd "C-<left>")  'windmove-left)
 (global-set-key (kbd "C-<down>")  'windmove-down)
 (global-set-key (kbd "C-<up>")    'windmove-up)
 (global-set-key (kbd "C-<right>") 'windmove-right)
-; kill windows
+; set kill windows to C-w
 (global-set-key (kbd "C-w") 'delete-window)
