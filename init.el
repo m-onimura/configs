abc18dff2d8dc0c0e587a62a3dcc408fccec6f4b
m-onimura <onimu99@gmail.com>
2022-07-03 14:11:02 +0900

    move defunc

--- init.el
+++ init.el
@@ -8,16 +8,8 @@
 ; set backspace to C-h
 (global-set-key "\C-h" `delete-backward-char)
 ; set wsl-copy to C-c
-(defun wsl-copy (start end)
-  (interactive "r")
-  (shell-command-on-region start end "clip.exe")
-  (deactivate-mark))
 (global-set-key "\C-c" `wsl-copy)
 ; set wsl-cut to C-x C-x
-(defun wsl-cut (start end)
-  (interactive "r")
-  (shell-command-on-region start end "clip.exe")
-  (kill-region start end))
 (global-set-key "\C-x\C-x" `wsl-cut)
 ; set isearch-forward to C-f C-f
 (global-set-key "\C-x\C-f" `isearch-forward)
@@ -30,3 +22,14 @@
 (global-set-key (kbd "C-<right>") 'windmove-right)
 ; set kill windows to C-w
 (global-set-key (kbd "C-w") 'delete-window)
+
+; copy to windows clipboard
+(defun wsl-copy (start end)
+  (interactive "r")
+  (shell-command-on-region start end "clip.exe")
+  (deactivate-mark))
+; cut to windows clipboard
+(defun wsl-cut (start end)
+  (interactive "r")
+  (shell-command-on-region start end "clip.exe")
+  (kill-region start end))
